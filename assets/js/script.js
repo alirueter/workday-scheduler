c623a1a5d9a0999a4207e5bfbc0d7c4de10123ca
alirueter <rueter.alison@gmail.com>
2020-06-27 09:29:39 -0500

    relinked stylesheet and added comments to plan next steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,33 @@ const getDateAndTime = () => {
     dateElement.innerHTML = dateAndTime;
 };
 
-//save events to local storage
+//color code time blocks to represent if time is in past, present or future
+var checkTime = function () {
+    var currentTime = moment.hour();
+    if (currentTime >= 8 && currentTime < 9) {
+        $(eightAM).addClass("present");
+      }
+    else if (currentTime >= 9 && currentTime <10){
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("present");
+    }
+    else if (currentTime >=10 && currentTime < 11){
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("present");
+    }
+    //continue through full workday, background colors not changing?
+    //or use .isBefore .isAfter .isBetween? but just check hour? 
+};
+
+//save events to local storage when save button is clicked (JSON stringify?)
 
 
+
+//when page is refreshed, then saved events persist (JSON parse?)
+
+
+
+//call functions
 getDateAndTime();
+checkTime();
