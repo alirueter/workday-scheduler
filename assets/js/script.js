ae93983b8981cc3c79e1b01d957c413315deaee0
alirueter <rueter.alison@gmail.com>
2020-06-27 11:22:04 -0500

    color codes, setInterval(), localStorage.setItem()

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,16 @@ var twoPM = document.getElementById("2PM");
 var threePM = document.getElementById("3PM");
 var fourPM = document.getElementById("4PM");
 var fivePM = document.getElementById("5PM");
-//use counter/iterate through instead? make list items instead of divs/rows/columns?
+
+
+//save events to local storage when save button is clicked 
+$(".saveBtn").on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var time = $(this).attr("id");
+
+    localStorage.setItem("event", text, time);
+    
+});
 
 //display date and time at top of page
 const getDateAndTime = () => {
@@ -21,31 +30,140 @@ const getDateAndTime = () => {
 
 //color code time blocks to represent if time is in past, present or future
 var checkTime = function () {
-    var currentTime = moment.hour();
+    var currentTime = moment().hours();
     if (currentTime >= 8 && currentTime < 9) {
         $(eightAM).addClass("present");
-      }
-    else if (currentTime >= 9 && currentTime <10){
+        $(nineAM).addClass("future");
+        $(tenAM).addClass("future");
+        $(elevenAM).addClass("future");
+        $(twelvePM).addClass("future");
+        $(onePM).addClass("future");
+        $(twoPM).addClass("future");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 9 && currentTime <10) {
         $(eightAM).addClass("past");
         $(nineAM).addClass("present");
+        $(tenAM).addClass("future");
+        $(elevenAM).addClass("future");
+        $(twelvePM).addClass("future");
+        $(onePM).addClass("future");
+        $(twoPM).addClass("future");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
     }
-    else if (currentTime >=10 && currentTime < 11){
+    else if (currentTime >= 10 && currentTime < 11) {
         $(eightAM).addClass("past");
         $(nineAM).addClass("past");
         $(tenAM).addClass("present");
+        $(elevenAM).addClass("future");
+        $(twelvePM).addClass("future");
+        $(onePM).addClass("future");
+        $(twoPM).addClass("future");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
     }
-    //continue through full workday, background colors not changing?
-    //or use .isBefore .isAfter .isBetween? but just check hour? 
+    else if (currentTime >= 11 &&currentTime < 12) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("present");
+        $(twelvePM).addClass("future");
+        $(onePM).addClass("future");
+        $(twoPM).addClass("future");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 12 &&currentTime < 13) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("past");
+        $(twelvePM).addClass("present");
+        $(onePM).addClass("future");
+        $(twoPM).addClass("future");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 13 &&currentTime < 14) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("past");
+        $(twelvePM).addClass("past");
+        $(onePM).addClass("present");
+        $(twoPM).addClass("future");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 14 &&currentTime < 15) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("past");
+        $(twelvePM).addClass("past");
+        $(onePM).addClass("past");
+        $(twoPM).addClass("present");
+        $(threePM).addClass("future");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 15 &&currentTime < 16) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("past");
+        $(twelvePM).addClass("past");
+        $(onePM).addClass("past");
+        $(twoPM).addClass("past");
+        $(threePM).addClass("present");
+        $(fourPM).addClass("future");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 16 &&currentTime < 17) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("past");
+        $(twelvePM).addClass("past");
+        $(onePM).addClass("past");
+        $(twoPM).addClass("past");
+        $(threePM).addClass("past");
+        $(fourPM).addClass("present");
+        $(fivePM).addClass("future");
+    }
+    else if (currentTime >= 17 &&currentTime < 18) {
+        $(eightAM).addClass("past");
+        $(nineAM).addClass("past");
+        $(tenAM).addClass("past");
+        $(elevenAM).addClass("past");
+        $(twelvePM).addClass("past");
+        $(onePM).addClass("past");
+        $(twoPM).addClass("past");
+        $(threePM).addClass("past");
+        $(fourPM).addClass("past");
+        $(fivePM).addClass("present");
+    }
 };
-
-//save events to local storage when save button is clicked (JSON stringify?)
-
+checkTime();  
 
 
-//when page is refreshed, then saved events persist (JSON parse?)
+//set interval
+var int = setInterval(checkTime, (60*1000)*10);
+console.log("check interval", int);
 
+
+//when page is refreshed, then saved events persist
+$(".description #8AM").val(localStorage.getItem("#8AM"));
 
 
 //call functions
 getDateAndTime();
-checkTime();
+
